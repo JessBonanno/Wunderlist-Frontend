b98bae95afddba18ff5a1d7eb243885b81def57c
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-26 15:12:21 -0700

    added backend server to baseURL code in axiosWithAuth

--- wunderlist/src/utils/axiosWithAuth.js
+++ wunderlist/src/utils/axiosWithAuth.js
@@ -4,7 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    baseURL: "",
+    baseURL: "https://wunderlist-1-bw.herokuapp.com",
     headers: {
       Authorization: token,
     },
